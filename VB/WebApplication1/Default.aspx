aa7433cf5c044244badfa34c52c087c7d03fa07b
DevExpressExampleBot <fakeEmail>
2023-02-08 19:18:26 +0400

    Source auto update [skip ci]

--- VB/WebApplication1/Default.aspx
+++ VB/WebApplication1/Default.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Language="vb" AutoEventWireup="true" CodeBehind="Default.aspx.vb" Inherits="WebApplication1.Default" %>
+<%@ Page Language="VB" AutoEventWireup="true" CodeBehind="Default.aspx.vb" Inherits="WebApplication1.Default" %>
 
 <%@ Register assembly="DevExpress.XtraReports.v16.1.Web, Version=16.1.17.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a" namespace="DevExpress.XtraReports.Web" tagprefix="dx" %>
 
@@ -11,12 +11,12 @@
 <body>
     <form id="form1" runat="server">
     <div>
-
-
+    
+    
         <dx:ASPxReportDesigner ID="ASPxReportDesigner1" runat="server">
         </dx:ASPxReportDesigner>
-
-
+    
+    
     </div>
     </form>
 </body>
--- VB/WebApplication1/Default.aspx.vb
+++ VB/WebApplication1/Default.aspx.vb
@@ -1,119 +1,110 @@
-﻿Imports DevExpress.DataAccess.ConnectionParameters
+Imports DevExpress.DataAccess.ConnectionParameters
 Imports DevExpress.DataAccess.Sql
 Imports DevExpress.XtraReports.UI
 Imports System
 Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Web
 Imports System.Web.UI
 Imports System.Web.UI.WebControls
 
 Namespace WebApplication1
-    Partial Public Class [Default]
-        Inherits System.Web.UI.Page
+
+    Public Partial Class [Default]
+        Inherits Page
 
         Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
-
-            Dim connectionParameters As New MsSqlConnectionParameters(".", "NWind", Nothing, Nothing, MsSqlAuthorizationType.Windows)
-            Dim ds As New DevExpress.DataAccess.Sql.SqlDataSource(connectionParameters)
+            Dim connectionParameters As MsSqlConnectionParameters = New MsSqlConnectionParameters(".", "NWind", Nothing, Nothing, MsSqlAuthorizationType.Windows)
+            Dim ds As DevExpress.DataAccess.Sql.SqlDataSource = New DevExpress.DataAccess.Sql.SqlDataSource(connectionParameters)
             GenerateSqlDataSource(ds)
-
-            Dim objds As New DevExpress.DataAccess.ObjectBinding.ObjectDataSource()
+            Dim objds As DevExpress.DataAccess.ObjectBinding.ObjectDataSource = New DevExpress.DataAccess.ObjectBinding.ObjectDataSource()
             GenerateObjectDataSource(objds)
-
-            Dim efds As New DevExpress.DataAccess.EntityFramework.EFDataSource()
+            Dim efds As DevExpress.DataAccess.EntityFramework.EFDataSource = New DevExpress.DataAccess.EntityFramework.EFDataSource()
             GenerateEFDataSource(efds)
-
-            Dim exds As New DevExpress.DataAccess.Excel.ExcelDataSource()
+            Dim exds As DevExpress.DataAccess.Excel.ExcelDataSource = New DevExpress.DataAccess.Excel.ExcelDataSource()
             GenerateExcelDataSource(exds)
-
             ASPxReportDesigner1.DataSources.Add("CustomSqlDS", ds)
             ASPxReportDesigner1.DataSources.Add("CustomObjDs", objds)
             ASPxReportDesigner1.DataSources.Add("CustomEfDs", efds)
             ASPxReportDesigner1.DataSources.Add("CustomExcelDs", exds)
-
-            ASPxReportDesigner1.OpenReport(New DevExpress.XtraReports.UI.XtraReport())
+            ASPxReportDesigner1.OpenReport(New XtraReport())
         End Sub
 
         Private Sub GenerateExcelDataSource(ByVal exds As DevExpress.DataAccess.Excel.ExcelDataSource)
             exds.FileName = Server.MapPath("bin") & "\Categories.xlsx"
             exds.Name = "excelDataSource1"
-
-            Dim excelWorksheetSettings1 As New DevExpress.DataAccess.Excel.ExcelWorksheetSettings() With {.CellRange = Nothing, .WorksheetName = "Sheet"}
-            Dim excelSourceOptions1 As New DevExpress.DataAccess.Excel.ExcelSourceOptions(excelWorksheetSettings1) With {.SkipEmptyRows = True, .SkipHiddenColumns =True, .SkipHiddenRows = True, .UseFirstRowAsHeader = True}
-
+            Dim excelWorksheetSettings1 As DevExpress.DataAccess.Excel.ExcelWorksheetSettings = New DevExpress.DataAccess.Excel.ExcelWorksheetSettings() With {.CellRange = Nothing, .WorksheetName = "Sheet"}
+            Dim excelSourceOptions1 As DevExpress.DataAccess.Excel.ExcelSourceOptions = New DevExpress.DataAccess.Excel.ExcelSourceOptions(excelWorksheetSettings1) With {.SkipEmptyRows = True, .SkipHiddenColumns = True, .SkipHiddenRows = True, .UseFirstRowAsHeader = True}
             exds.SourceOptions = excelSourceOptions1
-
-            Dim fieldInfo1 As New DevExpress.DataAccess.Excel.FieldInfo() With {.Name = "CategoryID", .Type = GetType(Double)}
-            Dim fieldInfo2 As New DevExpress.DataAccess.Excel.FieldInfo() With {.Name = "CategoryName", .Type = GetType(String)}
-            Dim fieldInfo3 As New DevExpress.DataAccess.Excel.FieldInfo() With {.Name = "Description", .Type = GetType(String)}
-
-            exds.Schema.AddRange(New DevExpress.DataAccess.Excel.FieldInfo() { fieldInfo1, fieldInfo2, fieldInfo3 })
+            Dim fieldInfo1 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo() With {.Name = "CategoryID", .Type = GetType(Double)}
+            Dim fieldInfo2 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo() With {.Name = "CategoryName", .Type = GetType(String)}
+            Dim fieldInfo3 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo() With {.Name = "Description", .Type = GetType(String)}
+            exds.Schema.AddRange(New DevExpress.DataAccess.Excel.FieldInfo() {fieldInfo1, fieldInfo2, fieldInfo3})
             exds.RebuildResultSchema()
         End Sub
+
         Private Sub GenerateEFDataSource(ByVal efds As DevExpress.DataAccess.EntityFramework.EFDataSource)
-            DirectCast(efds, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(efds, System.ComponentModel.ISupportInitialize).BeginInit()
             efds.Name = "efDataSource1"
             efds.ConnectionParameters = ConfigureEfConnectionParameters()
-            DirectCast(efds, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(efds, System.ComponentModel.ISupportInitialize).EndInit()
         End Sub
+
         Private Function ConfigureEfConnectionParameters() As DevExpress.DataAccess.EntityFramework.EFConnectionParameters
-            Dim efConnParam As New DevExpress.DataAccess.EntityFramework.EFConnectionParameters()
-            efConnParam.ConnectionString = EFConnectionParams.Connection
+            Dim efConnParam As DevExpress.DataAccess.EntityFramework.EFConnectionParameters = New DevExpress.DataAccess.EntityFramework.EFConnectionParameters()
+            efConnParam.ConnectionString = Connection
             efConnParam.ConnectionStringName = ""
-            efConnParam.Source = EFConnectionParams.SourceType
+            efConnParam.Source = SourceType
             Return efConnParam
         End Function
+
         Private Shared Sub GenerateSqlDataSource(ByVal ds As DevExpress.DataAccess.Sql.SqlDataSource)
             ' Create an SQL query to access the Products table.
-            Dim query As New CustomSqlQuery()
+            Dim query As CustomSqlQuery = New CustomSqlQuery()
             query.Name = "customQuery1"
             query.Sql = "SELECT * FROM Products"
-
             ds.Queries.Add(query)
             ds.RebuildResultSchema()
         End Sub
+
         Private Shared Sub GenerateObjectDataSource(ByVal objds As DevExpress.DataAccess.ObjectBinding.ObjectDataSource)
             objds.BeginInit()
-
             objds.Name = "ObjSource"
             objds.DataSource = GetType(ItemList)
             objds.Constructor = New DevExpress.DataAccess.ObjectBinding.ObjectConstructorInfo()
-
             objds.EndInit()
         End Sub
     End Class
-    #Region "Object Data Source"
+
+#Region "Object Data Source"
     Public Class ItemList
         Inherits List(Of Item)
 
         Public Sub New()
-            For i As Integer = 0 To 9
+            For i As Integer = 0 To 10 - 1
                 Add(New Item() With {.Name = i.ToString()})
-            Next i
+            Next
         End Sub
     End Class
+
     Public Class Item
-        Public Property Name() As String
+
+        Public Property Name As String
     End Class
-    #End Region
-    #Region "EF Data Source "
-    Public NotInheritable Class EFConnectionParams
 
-        Private Sub New()
-        End Sub
+#End Region
+#Region "EF Data Source "
+    Public Module EFConnectionParams
 
-        Public Shared ReadOnly Property Connection() As String
+        Public ReadOnly Property Connection As String
             Get
                 Return "Data Source=localhost;Initial Catalog=Northwind;Persist Security Info=True;User ID=sa;Password=dx;MultipleActiveResultSets=True;Application Name=EntityFramework"
             End Get
         End Property
 
-        Public Shared ReadOnly Property SourceType() As Type
+        Public ReadOnly Property SourceType As Type
             Get
                 Return GetType(NorthwindEntities)
             End Get
         End Property
-    End Class
-    #End Region
+    End Module
+#End Region
 End Namespace
